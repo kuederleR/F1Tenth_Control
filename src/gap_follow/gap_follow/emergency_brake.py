30cc4054a934483e663f0541245a814371b27f01
Ryan Kuederle <ryan@rkuederle.com>
2025-11-13 17:48:29 -0500

    reverted to old controls

--- src/gap_follow/gap_follow/emergency_brake.py
+++ src/gap_follow/gap_follow/emergency_brake.py
@@ -4,6 +4,7 @@ from rclpy.node import Node
 from ackermann_msgs.msg import AckermannDriveStamped
 from sensor_msgs.msg import LaserScan
 from nav_msgs.msg import Odometry
+from std_msgs.msg import String
 import sys
 import termios
 import tty
@@ -15,10 +16,10 @@ ACCEPTABLE_TTC = 0.5 #s
 class KeyboardControlNode(Node):
     def __init__(self):
         super().__init__('keyboard_control')
-        self.publisher_ = self.create_publisher(AckermannDriveStamped, 'drive', 10)
+        self.publisher_ = self.create_publisher(AckermannDriveStamped, '/safety_node', 10)
         self.lidar_sub = self.create_subscription(
             LaserScan,
-            '/safety_node',
+            '/scan',
             self.laser_callback,
             10
         )
@@ -30,9 +31,13 @@ class KeyboardControlNode(Node):
             10
         )
 
+        self.info_publisher = self.create_publisher(String, 'emergency_brake_info', 10)
+
         self.speed = 0.0
         self.steering_angle = 0.0
         self.key_pressed = None
+
+        self.ttc = 0.0
 
         self.speed_odom = 0.0
         self.safety_brake_flag = 0.0
@@ -49,6 +54,7 @@ class KeyboardControlNode(Node):
         self.timer = self.create_timer(0.1, self.timer_callback)
 
     def laser_callback(self, msg):
+        self.get_logger().info('Laser callback triggered')
         laser_time = self.get_clock().now().nanoseconds
         laser_dist = self.fwd_range(msg)
         if self.ttc_params['last_scan_ns'] >= 0:
@@ -57,10 +63,11 @@ class KeyboardControlNode(Node):
                 velocity = self.speed_odom
                 ttc = laser_dist / velocity
 
-            except:
+            except Exception as e:
                 ttc = -1
 
             self.safety_brake_flag = ttc > 0 and ttc < ACCEPTABLE_TTC
+            self.ttc = ttc
 
         self.ttc_params['last_scan_dist'] = laser_dist
         self.ttc_params['last_scan_ns'] = laser_time
@@ -74,7 +81,7 @@ class KeyboardControlNode(Node):
             angle = self.angle(msg.angle_min, msg.angle_max, i, len(ranges))
             y_dist = ranges[i] * math.sin(angle)
             x_dist = ranges[i] * math.cos(angle)
-            if -self.vehicle_width_m / 2 < y_dist and y_dist < self.vehicle_width_m / 2:
+            if -self.vehicle_width_m / 2 < y_dist and y_dist < self.vehicle_width_m / 2 and x_dist > 0.1:
                 count_fwd += 1
                 fwd_ranges.append(i)
                 if ranges[i] < min_dist:
@@ -98,7 +105,10 @@ class KeyboardControlNode(Node):
         return index
 
     def timer_callback(self):
-        if self.safety_brake_flag:
+        info_msg = String()
+        info_msg.data = f'Last Scan Distance: {self.ttc_params["last_scan_dist"]}, Safety Brake: {self.safety_brake_flag}, TTC: {self.ttc}'
+        self.info_publisher.publish(info_msg)
+        if self.safety_brake_flag or self.ttc_params['last_scan_dist'] < 0.2:
                 self.speed = 0.0
                 msg = AckermannDriveStamped()
                 msg.drive.speed = self.speed
